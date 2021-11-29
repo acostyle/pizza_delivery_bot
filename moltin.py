452a2c35c7db04640a6a0e5fbb33470838fe229e
acostyle <acostyle.m.z@protonmail.com>
2021-11-29 19:37:36 +0600

    feat: add flow, field, entry creation functions

--- moltin.py
+++ moltin.py
@@ -12,7 +12,15 @@ CLIENT_SECRET = env.str('CLIENT_SECRET')
 
 
 def get_access_token(redis_db):
-    # it will be used in the future
+    """
+    Get access token and store it in DB.
+
+    Returns:
+        return: access token.
+
+    Args:
+        redis_db: database.
+    """
     access_token = redis_db.get('access_token')
     if not access_token:
         access_token, time_to_expire = get_auth_token()
@@ -26,6 +34,12 @@ def get_access_token(redis_db):
 
 
 def get_auth_token():
+    """
+    Create auth data.
+
+    Returns:
+        return: access token and token expiring time.
+    """
     payload = {
         'client_id': CLIENT_ID,
         'client_secret': CLIENT_SECRET,
@@ -41,6 +55,16 @@ def get_auth_token():
 
 
 def create_product(access_token, product):
+    """
+    Create a product with the defined attributes.
+
+    Returns:
+        return: ID of the product.
+
+    Args:
+        access_token: required to get access to the API.
+        product: piece of information about for the product.
+    """
     headers = {
         'Authorization': 'Bearer {0}'.format(access_token),
         'Content-type': 'application/json',
@@ -74,6 +98,16 @@ def create_product(access_token, product):
 
 
 def create_picture(access_token, picture):
+    """
+    Create picture for the product.
+
+    Returns:
+        return: ID of the picture.
+
+    Args:
+        access_token: required to get access to the API.
+        picture: the picture you want to upload.
+    """
     headers = {
         'Authorization': 'Bearer {0}'.format(access_token),
     }
@@ -91,6 +125,14 @@ def create_picture(access_token, picture):
 
 
 def link_picture_to_product(access_token, product_id, picture_id):
+    """
+    Create product relationship to the picture.
+
+    Args:
+        access_token: required to get access to the API.
+        product_id: the ID of the product you want to relate to the image.
+        picture_id: the ID of the image.
+    """
     headers = {
         'Authorization': 'Bearer {0}'.format(access_token),
         'Content-Type': 'application/json',
@@ -100,12 +142,111 @@ def link_picture_to_product(access_token, product_id, picture_id):
         'data': {
             'type': 'main_image',
             'id': picture_id,
-        }
+        },
     }
 
     api_url = '{0}/v2/products/{1}/relationships/main-image'.format(
         API_BASE_URL,
         product_id,
     )
+    response = requests.post(url=api_url, headers=headers, json=payload)
+    response.raise_for_status()
+
+
+def create_flow(access_token, name, description):
+    """
+    Create flow.
+
+    Args:
+        access_token: required to get access to the API.
+        name: specifies the name of the flow.
+        description: specifies the description for the flow.
+    """
+    headers = {
+        'Authorization': 'Bearer {0}'.format(access_token),
+        'Content-Type': 'application/json',
+    }
+
+    payload = {
+        'data': {
+            'type': 'flow',
+            'name': name,
+            'slug': slugify(name),
+            'description': description,
+            'enabled': True,
+        },
+    }
+
+    api_url = '{0}/v2/flows'.format(API_BASE_URL)
+    response = requests.post(url=api_url, headers=headers, json=payload)
+    response.raise_for_status()
+
+
+def create_flow_field(access_token, name, description, flow_id):
+    """
+    Create flow fields.
+
+    Args:
+        access_token: required to get access to the API.
+        name: specifies the name of the field.
+        description: specifies the description for this field.
+        flow_id: id for the flow to create the field.
+    """
+    headers = {
+        'Authorization': 'Bearer {0}'.format(access_token),
+        'Content-Type': 'application/json',
+    }
+
+    payload = {
+        'data': {
+            'type': 'field',
+            'name': name,
+            'slug': slugify(name),
+            'field_type': 'string',
+            'description': description,
+            'required': True,
+            'default': '',
+            'enabled': True,
+            'relationships': {
+                'flow': {
+                    'data': {
+                        'type': 'flow',
+                        'id': flow_id,
+                    },
+                },
+            },
+        },
+    }
+
+    api_url = '{0}/v2/fields'.format(API_BASE_URL)
+    response = requests.post(url=api_url, headers=headers, json=payload)
+    response.raise_for_status()
+
+
+def create_entry(access_token, entry_data, flow_slug):
+    """
+    Create entries in flow.
+
+    Args:
+        access_token: required to get access to the API.
+        entry_data: piece of information to create entry with needed data.
+        flow_slug: slug for the flow to create an entry.
+    """
+    headers = {
+        'Authorization': 'Bearer {0}'.format(access_token),
+        'Content-Type': 'application/json',
+    }
+
+    payload = {
+        'data': {
+            'type': 'entry',
+            'address': entry_data['address']['full'],
+            'alias': entry_data['alias'],
+            'longitude': entry_data['coordinates']['lon'],
+            'latitude': entry_data['coordinates']['lat'],
+        },
+    }
+
+    api_url = '{0}/v2/flows/{1}/entries'.format(API_BASE_URL, flow_slug)
     response = requests.post(url=api_url, headers=headers, json=payload)
     response.raise_for_status()
